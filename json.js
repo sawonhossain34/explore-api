bdca9709d023a104adcff33ca5e0cf8c0c6a13d5
sawonhossain34 <sawonhossain3491@gmail.com>
2023-02-22 21:38:29 +0600

    JSON stringify and JSON parse json file add

--- json.js
+++ json.js
@@ -1,4 +1,29 @@
 // javascrippt object notation
-const user = {id : 1, name:'Gorib Amir', job: 'actor'};
+// const user = {id : 1, name:'Gorib Amir', job: 'actor'};
+// const stringtified = JSON.stringify(user);
 
-console.log(user);
+// console.log(user);
+// console.log(stringtified);
+
+
+const shop = {
+    ownner : 'aliya',
+    address : {
+        street : 'moheshpur',
+        city : 'jessore',
+        country : 'BD'
+    },
+    products : ['laptop','mobail','mouse','keyboard'],
+    revinue : 45000,
+    isOpen : true,
+    isNew : false
+
+}
+// console.log(shop);
+const shopJSON = JSON.stringify(shop);
+const shopObj = JSON.parse(shopJSON);
+
+
+
+console.log(shopJSON);
+console.log(shopObj);
